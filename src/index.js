e11b53cb488902c18ecb9c29b07fd686391783e8
swanscon <swanscon@gmail.com>
2024-09-30 10:51:12 -0500

    Configure GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<DarkModeProvider>
-			<BrowserRouter>
+			<BrowserRouter basename="/cs-portfolio-web">
 				<App />
 			</BrowserRouter>
 		</DarkModeProvider>
